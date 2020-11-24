4b521ba3889d69423a674cb00b9b0163cd63b024
lenglianping <llp@qq.com>
2020-11-24 17:37:36 +0800

    提交

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,9 +1,42 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function (options) {
-  console.log(options);
+$.ajaxPrefilter(function (ajaxOpt) {
+  console.log(ajaxOpt);
 
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  ajaxOpt.url = 'http://ajax.frontend.itheima.net' + ajaxOpt.url;
+
+
+  // b.为所有 /my/请求添加 token
+  if (ajaxOpt.url.indexOf('/my/') > -1) {
+    ajaxOpt.headers = {
+      Authorization: localStorage.getItem('token')
+
+    }
+  }
+
+
+  // c.为所有的ajax请求 统一配置complete 事件函数................
+  ajaxOpt.complete = function (res) {
+    if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+      // c2.木有登录，则：
+      // c2.1 显示需要重新登录的消息，显示结束后，再执行 清空token和跳转操作
+      layer.msg(res.responseJSON.message, {
+        icon: 1,
+        time: 1500 //2秒关闭（如果不配置，默认是3秒）
+      }, function () {
+        // 清空token
+        localStorage.removeItem('token');
+        //跳转到login.html
+        location.href = '/login.html'
+
+      });
+
+
+    }
+
+
+
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,66 @@
+// 1.在dom树 创建完后 开始执行
+
+$(function () {
+
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'get',
+        headers: {
+            Authorization: localStorage.getItem('token')
+
+        },
+        success(res) {
+            console.log(res);
+
+            if (res.status != 0) return layui.layer.msg(res.message);
+            // 渲染洪湖信息头像
+            renderAvatar(res.data)
+        }
+    });
+    // 渲染用户信息函数...........................................................
+    function renderAvatar(usrData) {
+        // a. 先获取用户名（昵称/登录名）
+        let usrName = usrData.nickname || usrData.username;
+
+
+        // b.设置给welcomme span标签
+        $('#welcome').html(usrName);
+
+        // c.渲染头像
+        if (usrData.user_pic != null) {
+            // 有图片头像
+            $('.layui-nav-img').attr('src', usrData.user_pic).show();
+            // 隐藏文字头像
+            $('.text-avater').hide();
+        } else {
+            // 没有图片头像，使用文本头像
+            $('.layui-nav-img').hide();
+            // 获取名字首字母 大写
+            let firstChar = usrName[0].toUpperCase();
+            // 设置文字并显示
+            $('.text-avater').text(firstChar).show();
+
+
+        }
+
+    };
+
+    // 退出按钮函数
+
+    $('#btnLogout').on('click', function () {
+        // 弹出确认框
+        layui.layer.confirm('您确认退出系统吗？',
+            { icon: 3, title: '提示' },
+            function (index) {
+                // 删除 localStorage中的 token值
+                localStorage.removeItem('token');
+                // 跳转到 login.html
+                location.href = '/login.html'
+
+                layer.close(index);
+            });
+
+    })
+
+
+})
